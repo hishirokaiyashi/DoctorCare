63270cf6601d5346f05cf94e156ab1dcec0d0a46
hishirokaiyashi <hishirokaiyashi@gmail.com>
2023-01-08 21:31:47 +0700

    Doctor care

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+export { default as Nav } from "./Nav";
+export { default as HeroSection } from "./HeroSection";
+export { default as ServicosSection } from "./ServicosSection";
+export { default as SobreSection } from "./SobreSection";
+export { default as ContatoSection } from "./ContatoSection";
+export { default as Footer } from "./Footer";
+
+
+
+
+
